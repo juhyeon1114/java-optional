8a580fa10c6fb3807e1fd3c862300cdbf9664d02
juhyeon <juhyeon1114@gmail.com>
2024-07-24 22:16:58 +0900

    init

---
+++ src/main/java/study/javaoptional/JavaOptionalApplication.java
@@ -0,0 +1,13 @@
+package study.javaoptional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOptionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaOptionalApplication.class, args);
+	}
+
+}
